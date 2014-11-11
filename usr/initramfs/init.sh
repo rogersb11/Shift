6f9cf7ca1993c28aee1466b81679df4627d1d9da
rogersb11 <brettrogers11@gmail.com>
2014-11-10 21:58:14 -0500

    mainly some clean, random changes otherwise

--- usr/initramfs/init.sh
+++ usr/initramfs/init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 RAMDISK="/home/brett/kern/usr/initramfs/ramdisk"
-RAMDISK_OUT="/home/brett/kern/usr/initramfs/newramdisk.cpio"
+RAMDISK_OUT="/home/brett/kern/usr/initramfs/ramdisk.cpio"
 
 
 echo "Making ramdisk..."
 		cd ${RAMDISK}
-		rm *.cpio
+		rm ramdisk.cpio
 		find . -not -name ".gitignore" | cpio -o -H newc > ${RAMDISK_OUT}
 
 echo "Done!"

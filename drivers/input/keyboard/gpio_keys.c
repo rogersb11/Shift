52e4a968051a33c2c25ca15ecf16b0847f08f008
rogersb11 <brettrogers11@gmail.com>
2014-11-12 18:08:18 -0500

    Revert Slide2Wake, for testing

--- drivers/input/keyboard/gpio_keys.c
+++ drivers/input/keyboard/gpio_keys.c
@@ -26,7 +26,6 @@
 #include <linux/workqueue.h>
 #include <linux/gpio.h>
 #include <linux/irqdesc.h>
-#include <linux/i2c/mxt224_u1.h>
 #ifdef CONFIG_TOUCH_WAKE
 #include <linux/touch_wake.h>
 #endif
@@ -889,7 +888,6 @@ static int __devinit gpio_keys_probe(struct platform_device *pdev)
 
 		input_set_capability(input, type, button->code);
 
-		slide2wake_setdev(input);
 	}
 
 	error = sysfs_create_group(&pdev->dev.kobj, &gpio_keys_attr_group);
